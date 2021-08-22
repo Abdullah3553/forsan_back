4588ab36409f765e55eb870ce0cea87ed46f09f7
AhmedGamal77823 <ahmedgamale.1019@gmail.com>
2021-08-22 11:57:40 +0200

    updates

--- src/csv/csv.module.ts
+++ src/csv/csv.module.ts
@@ -3,10 +3,11 @@ import { CsvController } from './csv.controller';
 import {CSVService} from "./csv.service";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Player} from "../players/entities/players.entity";
+import { ActivityPlayer } from 'src/activityPlayers/entities/activityPlayers.entity';
 
 @Module({
   imports: [
-      TypeOrmModule.forFeature([Player])
+      TypeOrmModule.forFeature([Player, ActivityPlayer])
   ],
   controllers: [CsvController],
   providers: [CSVService]
